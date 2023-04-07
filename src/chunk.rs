4a0dab76c7988edfd618812a88f9124600b27fa4
Jonathan Paprocki <drbeefsupreme@discordja.net>
2023-04-07 11:39:25 -0400

    disassemble

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,3 +1,47 @@
+#[derive(Debug)]
 pub enum OpCode {
-    OP_RETURN,
+    OpReturn = 0,
+}
+
+impl From<u8> for OpCode {
+    fn from(code: u8) -> Self {
+        match code {
+            0 => OpCode::OpReturn,
+            _ => unimplemented!("Invalid OpCode"),
+        }
+    }
+}
+
+impl From<OpCode> for u8 {
+    fn from(code: OpCode) -> Self {
+        code as u8
+    }
+}
+
+#[derive(Debug)]
+pub struct Chunk {
+    code: Vec<u8>,
+}
+
+impl Chunk {
+    pub fn new() -> Chunk {
+        Chunk {
+            code: Vec::<u8>::new(),
+        }
+    }
+
+    pub fn write_opcode(&mut self, byte: OpCode) {
+        self.code.push(byte.into());
+    }
+
+    pub fn free(&mut self) {
+        self.code = Vec::<u8>::new();
+    }
+
+    pub fn disassemble(&self) -> Vec<OpCode> {
+        self.code.clone()
+                 .into_iter()
+                 .map(|v| v.into())
+                 .collect()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
-use std::env::args;
+mod chunk;
+mod memory;
 
-pub mod chunk;
+use std::env::args;
+use chunk::*;
+
 
 fn main() {
     let args: Vec<String> = args().collect();
+
+    let mut chunk = Chunk::new();
+
+    chunk.write_opcode(OpCode::OpReturn);
+
+    println!("{:?}", chunk.disassemble());
 }
